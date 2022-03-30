51e94ee92b5b53883c6e201f46f69a5d05ca58f8
rfgrfrgr <102197654+rfgrfrgr@users.noreply.github.com>
2022-03-30 01:28:26 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import telegram.ext
+import os
+
+with open('token.txt', 'r') as f:
+ TOKEN = f.read()
+
+def start(update, context):
+    update.message.reply_text("hello! swagat hai")
+    
+def a1(update, context):
+        zipk = context.args[0]
+        open('1.txt','w').write(zipk)
+        update.message.reply_text(f"started")
+        url=
+        os.system('python start.py ovh (url) 1 250 socks5.txt 100 172800')
+        zkm = open('2.txt', 'r').read()
+        update.message.reply_text(f"A1 is free now")
+
+
+updater = telegram.ext.Updater(TOKEN, use_context=True)
+disp = updater.dispatcher
+disp.add_handler(telegram.ext.CommandHandler("start", start))
+disp.add_handler(telegram.ext.CommandHandler("a1", a1))
+
+updater.start_polling()
+updater.idle()
